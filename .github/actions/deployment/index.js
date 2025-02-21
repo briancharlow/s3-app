207b191f275e83827835b9680bf420957cbbf28d
Charlow <briankyalo416@gmail.com>
2025-02-21 18:27:40 +0300

    Refactor GitHub Actions for improved deployment and testing workflow

--- .github/actions/deployment/index.js
+++ .github/actions/deployment/index.js
@@ -1,20 +1,16 @@
-const core= require('@actions/core') // get inputs and set otputs
-const exec= require('@actions/exec') // interact wththe cli
+const core = require('@actions/core');
+const exec = require('@actions/exec');
 
-function run(){
-    
-    //Get inputs
+async function run() {
+    const bucket = core.getInput('bucketName', { required: true });
+    const bucketRegion = core.getInput('bucketRegion', { required: true });
+    const distFiles = core.getInput('distFiles', { required: true });
 
-    const bucket = core.getInput('bucketName', {required:true})
-    const bucketRegion = core.getInput('bucketRegion', {required:true})
-    const distFiles = core.getInput('distFiles', {required:true})
+    const s3Url = `s3://${bucket}`;
+    await exec.exec(`aws s3 sync ${distFiles} ${s3Url} --region ${bucketRegion}`);
 
-    // upload
-    const s3Url= `s3://${bucket}`
-    exec.exec(` aws s3 sync ${distFiles} ${s3Url} --region ${bucketRegion}`)
-    
-   const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
-   core.setOutput("URL", websiteURL) //>> echo "URl=fvvv" >> $GITHUB_OUTPUT
+    const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput("URL", websiteURL);
 }
 
-run()
+run();
