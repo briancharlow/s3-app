53c15f5dd8424fc58898db5890e597dce75d0068
Charlow <briankyalo416@gmail.com>
2025-02-21 18:30:39 +0300

    Comment out Karma configuration in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,16 @@
-module.exports = function (config) {
-    config.set({
+// module.exports = function (config) {
+//     config.set({
      
-      reporters: ['progress', 'json'],
-      jsonReporter: {
-        stdout: false,
-        outputFile: 'test.json'
-      },
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-json-reporter')
-      ]
+//       reporters: ['progress', 'json'],
+//       jsonReporter: {
+//         stdout: false,
+//         outputFile: 'test.json'
+//       },
+//       plugins: [
+//         require('karma-jasmine'),
+//         require('karma-chrome-launcher'),
+//         require('karma-json-reporter')
+//       ]
       
-    });
-  };
+//     });
+//   };
